c2912130f0007fd19e7fcc31106ade9cd42c33c0
Shashank kumar Patwa <59040037+nshnkzz@users.noreply.github.com>
2020-10-31 09:07:03 +0530

    Uploaded new files
    These are the list of sitesthat I made during my Internship @ suven consultancy pvt. ltd. feel free  to make changes and for any issues I will be available to respond to your queries

---
+++ bloging site/javascript/initialise.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB9kfs-J8bxl6Kx9H4T6Qok6Ou4CclQFqk",
+    authDomain: "newexperiment-61894.firebaseapp.com",
+    databaseURL: "https://newexperiment-61894.firebaseio.com",
+    projectId: "newexperiment-61894",
+    storageBucket: "newexperiment-61894.appspot.com",
+    messagingSenderId: "28684039460",
+    appId: "1:28684039460:web:13c6d7485e49e9b431efb1",
+    measurementId: "G-GRXEYNNW4Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
